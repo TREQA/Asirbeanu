c828b30e166881b373c502b37a0f2fa73fb6a4c8
Asirbeanu <34644135+Asirbeanu@users.noreply.github.com>
2017-12-27 16:30:23 +0200

    Add files via upload

---
+++ Tema1.java
@@ -0,0 +1,23 @@
+public class Tema1 {
+
+    public static void main(String[] args) {
+        int operand1 = 2;
+        // valoarea operand-ului 1
+        int operand2 = 3;
+        // valoarea operand-ului 2
+        int sum = operand1 + operand2;
+        //suma valorii operanzilor 1 si 2
+        System.out.println(sum);
+        //afisarea sumei
+        int a=2,b=3;
+
+        /*System.out.print("First operand: ");
+        System.out.println(a);
+        System.out.print("Second operand: ");
+        System.out.println(b);
+        System.out.print("Sum: ");
+        */
+        System.out.println(a+b);
+    }
+}
+
